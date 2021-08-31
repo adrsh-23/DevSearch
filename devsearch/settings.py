f749a92498c509c96eea9f6792fee035beda39a3
adrsh23 <adarshthakurcr7@gmail.com>
2021-08-31 23:42:24 +0530

    add heroku files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6&^x=k9i--28@=5pc%d=73&6wtm3&ywh_6#g*)5-x85ucnc9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developersearch.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
